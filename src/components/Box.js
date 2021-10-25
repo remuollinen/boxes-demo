d865c93eb94fc7f818bc7378ae534d343dbddefa
Remu Ollinen <remu.ollinen@gmail.com>
2021-10-25 13:38:19 +0300

    added click handler

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,9 +1,14 @@
 const Box = (props) => {
+	const changeNameHandler = () => {
+		console.log("WOW from box");
+	};
+
 	return (
 		<div className="box">
 			<h2>{props.name}</h2>
 			<p>Title: {props.title}</p>
 			<p>Age: {props.age}</p>
+			<button onClick={changeNameHandler}>Click me!</button>
 		</div>
 	);
 };
--- src/components/MainC.js
+++ src/components/MainC.js
@@ -6,18 +6,18 @@ class MainC extends Component {
 		persons: [
 			{ name: "Maria", age: 29, title: "CEO" },
 			{ name: "Kati", age: 25, title: "Developer" },
-			{ name: "Katrin", age: 45, title: "Designer" },
+			{ name: "Leila", age: 45, title: "Designer" },
 		],
 	};
+
+	changeNameHandler = () => {
+		console.log("WOW");
+	};
+
 	render() {
 		return (
 			<main>
-				{this.state.persons.map((person) => {
-					return (
-						<Box name={person.name} title={person.title} age={person.age} />
-					);
-				})}
-				{/* <Box
+				<Box
 					name={this.state.persons[0].name}
 					title={this.state.persons[0].title}
 					age={this.state.persons[0].age}
@@ -31,7 +31,13 @@ class MainC extends Component {
 					name={this.state.persons[2].name}
 					title={this.state.persons[2].title}
 					age={this.state.persons[2].age}
-				/> */}
+				/>
+				{/* {this.state.persons.map((person) => {
+						return (
+							<Box name={person.name} title={person.title} age={person.age} />
+						);
+					})} */}
+				<button onClick={this.changeNameHandler}>Click me!</button>
 			</main>
 		);
 	}
